e1e2efc5f8e0f7b29aeef11eb872e78da9dfbf8c
davidwt5 <davidwtan75@gmail.com>
2024-05-01 13:00:46 +1000

    Basic Socket.io connection
    - Server able to log that the react app has connected to its socket

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,24 @@
-const express = require('express')
+import express from 'express';
+import { Server } from 'socket.io';
+import { createServer } from 'node:http';
+
+const port = 8090
 const app = express()
-const port = 3000
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+server.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { io } from 'socket.io-client';
+import {useEffect} from "react";
+
+const socket = io("http://localhost:8090", {
+  autoConnect: false
+});
 
 function App() {
+  useEffect(() => {
+    socket.connect();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div></div>
   );
 }
 
